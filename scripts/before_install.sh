9c9d22ca24451f6bfb1719956ffe18304e6370e4
Waqas Kayani <mvickykayani@gmail.com>
2023-10-12 01:03:39 +0500

    Update before_install.sh to do cleanup of target directory

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,18 +5,14 @@ if ! command -v java &> /dev/null
 then
     wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add - 
     sudo add-apt-repository 'deb https://apt.corretto.aws stable main'
-    sudo apt-get install -y java-1.8.0-amazon-corretto-jdk
+    sudo apt-get install -y java-1.8.0-amazon-corretto-jdk maven
 fi
 
-# Creating a backup of Jar binary
 DESTINATION_PATH="/home/ubuntu/app-deployment"
-BACKUP_PATH="/home/ubuntu/backup-binaries"
 
-# Create a backup directory if it doesn't exist
-mkdir -p $BACKUP_PATH
-
-# If there's an existing JAR, backup it
-if [ -f "${DESTINATION_PATH}/petclinic.war" ]; then
-    cp "${DESTINATION_PATH}/petclinic.war" "${BACKUP_PATH}/petclinic-$(date +%F-%T).war"
+# If there's an existing target directory
+if [ -d "${DESTINATION_PATH}/target" ]; then
+    cd ${DESTINATION_PATH}
+    sudo mvn clean
 fi
-echo "Successfully created backup of last deployment binary.."
+echo "Successfully cleaned up target path.."
