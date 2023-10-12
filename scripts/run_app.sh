414f855a5a3281756758cb628fb2b62c026e922a
Waqas Kayani <mvickykayani@gmail.com>
2023-10-12 12:47:13 +0500

    Update run_app.sh

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
 DESTINATION_PATH="/home/ubuntu/app-deployment"
-
-# Fetch parameters from AWS SSM Parameter Store
-echo "Fetching parameters from AWS SSM Parameter store..."
-DB_HOST=$(aws ssm get-parameter --name "/dev/database/host" --with-decryption --query "Parameter.Value" --output text)
-DB_NAME=$(aws ssm get-parameter --name "/dev/database/db_name" --with-decryption --query "Parameter.Value" --output text)
-DB_USER=$(aws ssm get-parameter --name "/dev/database/user" --with-decryption --query "Parameter.Value" --output text)
-DB_PASSWORD=$(aws ssm get-parameter --name "/dev/database/password" --with-decryption --query "Parameter.Value" --output text)
-if [ $? -eq 0 ]; then
-    echo "Fetched parameters successfully..."
-else
-    echo "Problem fetching parameters, please look into configurations.."
-fi
-
-# Export the variables and run application
-export DB_HOST
-export DB_NAME
-export DB_USER
-export DB_PASSWORD
+ENV_FILE="/usr/bin/pet-clinic.env"
 
 # Add the command to start your application here; for example:
 cd $DESTINATION_PATH
-sudo mvn tomcat7:run -DDB_HOST=$DB_HOST -DDB_USER=$DB_USER -DDB_PASSWORD=$DB_PASSWORD -DDB_NAME=$DB_NAME
+sudo mvn tomcat7:run
