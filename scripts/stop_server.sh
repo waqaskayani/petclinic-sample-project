d94d7642383f1335b2ace94931f2d4612a44273c
Waqas Kayani <mvickykayani@gmail.com>
2023-10-12 10:41:30 +0500

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-echo "Attempting to stop the server..."
 DESTINATION_PATH="/home/ubuntu/app-deployment"
+
+# Updating permission for current user
+CURRENT_USER=$(whoami)
+sudo chown -R $CURRENT_USER:$CURRENT_USER $DESTINATION_PATH
+
 SERVICE_NAME="petclinic-app"
 
+echo "Attempting to stop the server..."
 if systemctl list-units --full -all | grep -Fq "$SERVICE_NAME.service"; then
     echo "Service $SERVICE_NAME exists. Attempting to stop..."
 
