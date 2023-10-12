1284b11b878c3236d797c41963ad9b49e3ec2164
Waqas Kayani <mvickykayani@gmail.com>
2023-10-12 12:50:29 +0500

    Update run_app.sh

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 DESTINATION_PATH="/home/ubuntu/app-deployment"
-ENV_FILE="/usr/bin/pet-clinic.env"
 
 # Add the command to start your application here; for example:
 cd $DESTINATION_PATH
