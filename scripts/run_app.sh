d1f62bb2e653fa670eb926334ecad8363a43a185
Waqas Kayani <mvickykayani@gmail.com>
2023-10-12 10:11:33 +0500

    Update run_app.sh

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -3,10 +3,16 @@
 DESTINATION_PATH="/home/ubuntu/app-deployment"
 
 # Fetch parameters from AWS SSM Parameter Store
+echo "Fetching parameters from AWS SSM Parameter store..."
 DB_HOST=$(aws ssm get-parameter --name "/dev/database/host" --with-decryption --query "Parameter.Value" --output text)
 DB_NAME=$(aws ssm get-parameter --name "/dev/database/db_name" --with-decryption --query "Parameter.Value" --output text)
 DB_USER=$(aws ssm get-parameter --name "/dev/database/user" --with-decryption --query "Parameter.Value" --output text)
 DB_PASSWORD=$(aws ssm get-parameter --name "/dev/database/password" --with-decryption --query "Parameter.Value" --output text)
+if [ $? -eq 0 ]; then
+    echo "Fetched parameters successfully..."
+else
+    echo "Problem fetching parameters, please look into configurations.."
+fi
 
 # Export the variables and run application
 export DB_HOST
